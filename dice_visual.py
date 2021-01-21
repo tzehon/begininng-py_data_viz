cd6a231ca6e52e789921afa96bed5378e83d692b
tth <tzehon.tan@gmail.com>
2021-01-21 19:45:06 +0800

    Roll 2 D6 dice

---
+++ dice_visual.py
@@ -0,0 +1,29 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
+from die import Die
+
+die_1 = Die()
+die_2 = Die()
+
+results = []
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# Analyze results
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize results
+x_values = list(range(2, max_result+1))
+data = [Bar(x=x_values, y = frequencies)]
+
+x_axis_config = {'title': 'Result', 'dtick': 1}
+y_axis_config = {'title': 'Frequency of result'}
+my_layout = Layout(title='Results of rolling two D6 dice 10000 times',
+    xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
