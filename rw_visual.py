dd4f474be8f382e02e53a32728338324453f4869
tth <tzehon.tan@gmail.com>
2021-01-21 19:18:55 +0800

    Continuously generate random walks

--- rw_visual.py
+++ rw_visual.py
@@ -2,10 +2,16 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
+# Keep making new walks as long as program is active
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
 
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break;
