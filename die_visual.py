921439036fe6395cb56daf8e825a46aff0dc710f
tth <tzehon.tan@gmail.com>
2021-01-21 19:32:33 +0800

    Count frequencies

--- die_visual.py
+++ die_visual.py
@@ -3,8 +3,13 @@ from die import Die
 die = Die()
 
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
