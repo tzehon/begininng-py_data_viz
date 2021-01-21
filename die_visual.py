a62a2a684f94b8482d6b0e8e29f09e4230438a3a
tth <tzehon.tan@gmail.com>
2021-01-21 19:42:05 +0800

    Add visualization

--- die_visual.py
+++ die_visual.py
@@ -1,3 +1,6 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
 die = Die()
@@ -7,9 +10,18 @@ for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
+# Analyze results
 frequencies = []
 for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
+# Visualize results
+x_values = list(range(1, die.num_sides+1))
+data = [Bar(x=x_values, y = frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of result'}
+my_layout = Layout(title='Results of rolling one D6 10000 times',
+    xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
