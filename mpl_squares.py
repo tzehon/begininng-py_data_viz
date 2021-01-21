07bb80e9a3c9f0c782d6a0d44cec5e98477b9aea
tth <tzehon.tan@gmail.com>
2021-01-21 15:53:29 +0800

    Set theme

--- mpl_squares.py
+++ mpl_squares.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
+
+plt.style.use('fivethirtyeight')
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth=3)
 
