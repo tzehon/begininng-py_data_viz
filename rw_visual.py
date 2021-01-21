9cc8ac7e8a0a09a9444df23c09625709e8e4bcbb
tth <tzehon.tan@gmail.com>
2021-01-21 19:21:30 +0800

    Colour the points

--- rw_visual.py
+++ rw_visual.py
@@ -9,9 +9,11 @@ while True:
 
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers,
+        cmap=plt.cm.Blues, edgecolors='none', s=15)
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
     if keep_running == 'n':
-        break;
+        break
